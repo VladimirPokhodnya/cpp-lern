d7e6739faba77cdae714f53cdf018404912f184c
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-31 12:23:16 +0700

    F

--- 2.5/F.cpp
+++ 2.5/F.cpp
@@ -7,32 +7,35 @@ int main() {
   size_t rows, cols, mine;
   std::cin >> rows >> cols >> mine;
 
-  std::vector<std::vector<int>> board(rows, std::vector<int>(cols, 0));
+  std::vector<std::vector<int>> board(rows + 2, std::vector<int>(cols + 2, 0));
 
   for (size_t i = 0; i < mine; i++) {
     size_t x, y;
     std::cin >> x >> y;
-    x--;
-    y--;
+    // x--;
+    // y--;
     board[x][y] = 9;
-    // board[x - 1][y - 1] += 1;
-    // board[x - 1][y] += 1;
-    // board[x - 1][y + 1] += 1;
-    // board[x][y - 1] += 1;
-    // board[x][y - 1] += 1;
-    // board[x - 1][y] += 1;
-    // board[x][y] += 1;
-    // board[x][y - 1] += 1;
+
+    board[x - 1][y + 1] += 1;
+    board[x - 1][y] += 1;
+    board[x - 1][y - 1] += 1;
+    board[x][y + 1] += 1;
+    board[x][y - 1] += 1;
+    board[x + 1][y + 1] += 1;
+    board[x + 1][y] += 1;
+    board[x + 1][y - 1] += 1;
   }
 
-  for (size_t i = 0; i < rows; ++i) {
-    for (size_t j = 0; j < cols; ++j) {
-      if (board[i][j] == 9) {
+  std::cout << std::endl;
+
+  for (size_t i = 1; i < rows + 1; ++i) {
+    for (size_t j = 1; j < cols + 1; ++j) {
+      if (board[i][j] > 8) {
         std::cout << '*';
       } else {
         std::cout << board[i][j];
       }
-      if (j < cols - 1) {
+      if (j < cols - 3) {
         std::cout << ' ';
       }
     }
