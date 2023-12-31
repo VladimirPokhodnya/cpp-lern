dc9e97215ec30796f88fe2c158517da98ca219cb
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2023-12-31 11:55:57 +0700

    F

--- 2.5/F.cpp
+++ 2.5/F.cpp
@@ -4,19 +4,39 @@
 
 int main() {
 
-  size_t m, n, k;
+  size_t rows, cols, mine;
+  std::cin >> rows >> cols >> mine;
 
-  struct xy {
-    int x;
-    int y;
-  };
+  std::vector<std::vector<int>> board(rows, std::vector<int>(cols, 0));
 
-  std::cin >> m >> n >> k;
+  for (size_t i = 0; i < mine; i++) {
+    size_t x, y;
+    std::cin >> x >> y;
+    x--;
+    y--;
+    board[x][y] = 9;
+    // board[x - 1][y - 1] += 1;
+    // board[x - 1][y] += 1;
+    // board[x - 1][y + 1] += 1;
+    // board[x][y - 1] += 1;
+    // board[x][y - 1] += 1;
+    // board[x - 1][y] += 1;
+    // board[x][y] += 1;
+    // board[x][y - 1] += 1;
+  }
 
-  std::vector<struct xy> coord;
-  coord.reserve(k);
-  for (size_t i = 0; i < k; i++) {
-    std::cin >> coord[i].x >> coord[i].y;
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
+      if (board[i][j] == 9) {
+        std::cout << '*';
+      } else {
+        std::cout << board[i][j];
+      }
+      if (j < cols - 1) {
+        std::cout << ' ';
+      }
+    }
+    std::cout << "\n";
   }
 
   return 0;
