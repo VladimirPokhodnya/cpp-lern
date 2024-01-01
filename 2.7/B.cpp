3b3c90916bf70bc34c76031e87936e1e7eed5307
VlagimirPokhodnya <pokhodnya-ngs.ru@yandex.ru>
2024-01-01 16:00:04 +0700

    2.7 A

---
+++ 2.7/B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string CommonPrefix(const std::vector<std::string> &words);
+
+int main() {
+  const std::vector<std::string> word(
+      // {"apple", "apricot", "application", "ap"});
+      {});
+
+  std::string res = CommonPrefix(word);
+
+  std::cout << res << '\n' << "len = " << res.size();
+  return 0;
+}
+
+std::string CommonPrefix(const std::vector<std::string> &words) {
+
+  std::string res = "";
+
+  if (words.size() == 0) {
+    return res;
+  }
+
+  size_t min = 0;
+  for (size_t i = 1; i < words.size(); ++i) {
+    if (words[i] < words[min]) {
+      min = i;
+    }
+  }
+
+  for (size_t j = 0; j < words[min].size(); ++j) {
+    for (size_t i = 0; i < words.size(); ++i) {
+      if (i != min) {
+        if (words[i][j] != words[min][j]) {
+          return res;
+        }
+      }
+      if (i == words.size() - 1) {
+        res += words[i][j];
+      }
+    }
+  }
+
+  return res;
+}
